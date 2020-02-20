29b8d63962089c41e7854bf17d0de64d044e6136
lwalther <lwalther@novareto.de>
2020-02-20 10:55:45 +0100

    testform, taetigkeit_form biologie_form added

--- setup.py
+++ setup.py
@@ -54,6 +54,7 @@ setup(
         'plone.api>=1.8.4',
         'plone.restapi',
         'plone.app.dexterity',
+        'plone.formwidget.autocomplete==1.4.0',
     ],
     extras_require={
         'test': [
